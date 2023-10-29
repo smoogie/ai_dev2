5f930f6070e62a2b4d7e49ce61339a45a6f37346
smoogie <lukasz.pawlowski.inf@gmail.com>
2023-10-29 18:50:07 +0100

    mock blogger endpoint on test server

--- test_server/app/tasks/answers/bloggerValidate.js
+++ test_server/app/tasks/answers/bloggerValidate.js
@@ -1,8 +1,20 @@
 const buildAnswerResponse = require("./buildAnswerResponse");
+const bloggerData = require('../data/bloggerData')
 const bloggerValidate = function(data) {
     const answer = data.answer
     let isAnswerCorrect = false
+    if (answer.length == bloggerData.blog.length) {
+        isAnswerCorrect = answer.reduce(
+            (areChaptersCorrect, chapter, index) => areChaptersCorrect && validateChapter(chapter, bloggerData.blog[index]),
+            true
+        )
+    }
     return buildAnswerResponse(isAnswerCorrect)
+}
+
+function validateChapter(chapter, description) {
+    //here we should validate if each chapter has correct text, but we simplify it to check length
+    return (typeof chapter === 'string' && chapter.length > 100)
 }
 
 module.exports = bloggerValidate
--- test_server/app/tasks/data/bloggerData.js
+++ test_server/app/tasks/data/bloggerData.js
@@ -1,6 +1,12 @@
 const bloggerData = {
     code: 0,
-    msg: ""
+    msg: "please write blog post for the provided outline",
+    blog: [
+        'Wstęp: kilka słów na temat historii pizzy',
+        'Niezbędne składniki na pizzę',
+        'Robienie pizzy',
+        'Pieczenie pizzy w piekarniku'
+    ]
 }
 
 module.exports = bloggerData
