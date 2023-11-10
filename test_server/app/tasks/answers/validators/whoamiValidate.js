bd6fb04ad8bf3e2de2869a40038df30be572bd16
smoogie <lukasz.pawlowski.inf@gmail.com>
2023-11-10 17:25:06 +0100

    whoami + scraper + search

--- test_server/app/tasks/answers/validators/whoamiValidate.js
+++ test_server/app/tasks/answers/validators/whoamiValidate.js
@@ -2,8 +2,9 @@ const buildAnswerResponse = require("./buildAnswerResponse");
 
 const whoamiValidate = function(data) {
     const answer = data.answer
-    //TODO: compare to real result
-    return buildAnswerResponse(true)
+    //TODO: compare to real result with AI
+    const isCorrect = answer.includes("Steve Jobs")
+    return buildAnswerResponse(isCorrect)
 }
 
 module.exports = whoamiValidate
